c016c24dbfe9c345bed50b15d083ffc6de9760bf
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-23 10:48:46 +0200

    feat: add constants module for game configuration
    This commit adds a new module called `constants.py` which contains the constants used in the asteroids game. It defines the screen width and height, asteroid properties such as minimum radius, number of kinds, spawn rate, and maximum radius.
    
    The addition of this module will make it easier to manage and update game configuration in a centralized location.

---
+++ constants.py
@@ -0,0 +1,20 @@
+SCREEN_WIDTH = 1280
+"""
+constants.py
+
+This module contains the constants used in the asteroids game.
+
+Attributes:
+	SCREEN_WIDTH (int): The width of the game screen.
+	SCREEN_HEIGHT (int): The height of the game screen.
+	ASTEROID_MIN_RADIUS (int): The minimum radius of an asteroid.
+	ASTEROID_KINDS (int): The number of different kinds of asteroids.
+	ASTEROID_SPAWN_RATE (float): The rate at which asteroids spawn in seconds.
+	ASTEROID_MAX_RADIUS (int): The maximum radius of an asteroid.
+"""
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
