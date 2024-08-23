b001f076672dee566c7274f642d524ac17cf8ee6
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-08-23 10:44:23 +0200

    update main

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
+
+# main function
+def main():
+    """
+    Function to start the asteroids game.
+    """
+    print(f"Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
