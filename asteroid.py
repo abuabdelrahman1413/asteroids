7a363e9170ef74e4d33a13d9d5c818e5a3ebea10
mohaamed <ms22abuabdelrahman@gmail.com>
2024-09-15 12:06:01 +0200

    Refactor Asteroid class to split into smaller asteroids upon destruction

--- asteroid.py
+++ asteroid.py
@@ -8,7 +8,6 @@ import math
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.angle = random.uniform(20 * (math.pi / 180), 50 * (math.pi / 180))
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
@@ -21,3 +20,15 @@ class Asteroid(CircleShape):
             self.kill()
             return
         else:
+            angle = random.uniform(20, 50)
+            velocity1 = self.velocity.rotate(angle)
+            velocity2 = self.velocity.rotate(-angle)
+            velocity1 = velocity1 * 1.2
+            velocity2 = velocity2 * 1.2
+            asteroid1 = Asteroid(self.position.x, self.position.y,
+                                 self.radius / 2)
+            asteroid2 = Asteroid(self.position.x, self.position.y,
+                                 self.radius / 2)
+            asteroid1.velocity = velocity1
+            asteroid2.velocity = velocity2
+            self.kill()
